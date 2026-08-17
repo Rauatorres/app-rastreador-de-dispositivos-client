b01a8905f4a3917a8b734d04d8255a3932c74eb2
rauatorres <rauatorres@gmail.com>
2026-08-17 11:39:53 -0300

    mostrando na tela dispositivos conectados

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,25 +1,46 @@
+import { useEffect, useState } from "react";
 import getConnectedDevices from "../api/get-connected-devices";
 import ConnectedDeviceCard from "../components/map/ConnectedDeviceCard";
 import type ConnectionData from "../model/connectionData";
 
 export default function MapPage() {
-  async function showConnectedDevices() {
-    const connectedDevices = (await getConnectedDevices("http://127.0.0.1/"))
-      .result! as ConnectionData[];
-    return connectedDevices.map((device) => {
-      return (
-        <ConnectedDeviceCard
-          name={device.name}
-          connectionId={device.connectionId}
-        />
-      );
-    });
+    const [connectedDevices, setConnectedDevices] = useState<ConnectionData[]>([])
+
+    useEffect(() => {
+      async function registerConnectedDevices(){
+        const getConnectedDevicesRequest = (await getConnectedDevices("http://192.168.0.115:3000/"))
+
+        if(getConnectedDevicesRequest.success){
+          setConnectedDevices(getConnectedDevicesRequest
+            .result! as ConnectionData[]
+          )
+          console.log(getConnectedDevicesRequest.msg)
+        } else{
+          console.log(getConnectedDevicesRequest.error)
+        }
+
+      }
+      registerConnectedDevices()
+    }, [])
+
+  function showConnectedDevices() {
+    
+        
+        return connectedDevices.map((device) => {
+          return (
+            <ConnectedDeviceCard
+            key={device.connectionId}
+              name={device.name}
+              connectionId={device.connectionId}
+            />
+          );
+        });
   }
 
   return (
     <main>
       <h2>Dispositivos Conectados</h2>
-      <div></div>
+      <div>{showConnectedDevices()}</div>
     </main>
   );
 }
