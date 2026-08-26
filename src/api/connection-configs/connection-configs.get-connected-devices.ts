839affb1a3c9b6753f11b88321ea15aa6bc045dc
rauatorres <rauatorres@gmail.com>
2026-08-26 15:28:14 -0300

    consertando tela de mapa
    corrigindo erro

--- src/api/connection-configs/connection-configs.get-connected-devices.ts
+++ src/api/connection-configs/connection-configs.get-connected-devices.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import type ApiResponse from "../../model/apiResponse";
 import { configs } from "../configs/configs";
 
 export default async function getConnectedDevices() {
