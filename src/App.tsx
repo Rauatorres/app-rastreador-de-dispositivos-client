8e598acff400ec82375ab437610e3a3dcb0a3e39
rauatorres <rauatorres@gmail.com>
2026-08-26 11:08:25 -0300

    voltando para configurações de deploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import Header from "./components/header/Header";
 import ConnectDevicePage from "./pages/ConnectDevicePage";
 import PageContext from "./shared/context/page/PageContext";
 import MapPage from "./pages/MapPage";
-import { useCookies } from "react-cookie";
+// import { useCookies } from "react-cookie";
 
 function App() {
   const { currentPage } = useContext(PageContext);
-  const [cookie] = useCookies(["connectionId"]);
+  // const [cookie] = useCookies(["connectionId"]);
 
   function showPage() {
     switch (currentPage) {
